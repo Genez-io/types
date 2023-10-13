3affa50fea0ac7ee9c6bca236f060fdc944ebc87
Bogdan Vlad <bogdan@genez.io>
2023-10-13 12:03:30 +0300

    Add support for genezio decorators.

--- index.ts
+++ index.ts
@@ -1,3 +1,42 @@
+export type GenezioDeployClassParameters = {
+    // Specify the type of protocol that is used to invoke the methods of 
+    // this class.
+    //
+    // For more information: https://docs.genez.io/genezio-documentation/method-types
+    type?: "jsonrpc"|"http"|"cron"
+}
+
+export type GenezioDeployMethodParameters = {
+    // Specify the type of protocol that is used to invoke this specific method 
+    //
+    // For more information: https://docs.genez.io/genezio-documentation/method-types
+    type?: "jsonrpc"|"http"|"cron"
+
+    // If type is set to "cron", this cronString value will specify how often the method
+    // will be called.
+    //
+    // A good tool for building cron strings is https://crontab.guru/.
+    cronString?: string
+}
+
+// Decorator that marks that a class should be deployed using genezio.
+export function GenezioDeploy(_dict: GenezioDeployClassParameters = {}) {
+    return (_ctor: Function) => {}
+}
+
+// Decorator that marks that a method should be deployed using genezio.
+export function GenezioMethod(_dict: GenezioDeployMethodParameters = {}) {
+    return function (_target: Object, _key: string, descriptor: TypedPropertyDescriptor<any>) {
+        let originalMethod = descriptor.value;
+
+        descriptor.value = function(...args: any[]) {
+            return originalMethod.apply(this, args);
+        };
+
+        return descriptor;
+    }
+}
+
 export type GenezioHttpRequest  = {
   headers: { [key: string]: string };
   http: {
